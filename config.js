9cdbdad8296af5f89792f6028d33e615a30daf0e
Blaine <blaineturley@gmail.com>
2021-05-05 11:45:30 -0400

    ts

--- config.js
+++ config.js
@@ -8,12 +8,10 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "baron_test"
-      : process.env.DATABASE_URL || "baron";
-}
+  return process.env.DATABASE_URL || "baron";
+};
 
-const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+const BCRYPT_WORK_FACTOR = 12;
 
 console.log("Baron Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
 const db = new Client({
-  connectionString: getDatabaseUri()
+  connectionString: getDatabaseUri(),
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect();
