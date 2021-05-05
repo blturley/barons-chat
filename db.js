eb017e2466697e87ea7575c0b502c48719a802df
Blaine <blaineturley@gmail.com>
2021-05-05 11:01:20 -0400

    troubleshooting

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 "use strict";
 
-/** Database setup for jobly. */
-
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
 const db = new Client({
   connectionString: getDatabaseUri(),
+  ssl:true
 });
 
 db.connect();
